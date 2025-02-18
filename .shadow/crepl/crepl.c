45790e0e202da276f3d5595dbb439ddd381dea82
tracer-nju <tracer@nju.edu.cn>
2025-02-18 10:40:38 +0800

    Linux LAPTOP-B1DV45O5 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:40:38 up  2:38,  1 user,  load average: 0.20, 0.09, 0.08

--- .shadow/crepl/crepl.c
+++ .shadow/crepl/crepl.c
@@ -42,7 +42,7 @@ int main(int argc, char* argv[]) {
       char new_line[1100];
       snprintf(new_line, sizeof(new_line),
                "int __expr_wrapper_%d() {return %s;}", index, line);
-      printf("%s\n", new_line);
+      //printf("%s\n", new_line);
       //若编译成功，再执行
       if (complie_shared_lib(new_line) == 0) {
         snprintf(new_line, sizeof(new_line), "__expr_wrapper_%d", index);
