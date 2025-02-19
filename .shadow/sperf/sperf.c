33f93050ca3b23af42b78a0a17dccc24b2a67c56
tracer-nju <tracer@nju.edu.cn>
2025-02-19 12:26:35 +0800

    Linux LAPTOP-B1DV45O5 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:26:35 up  3:09,  1 user,  load average: 0.13, 0.09, 0.08

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -139,7 +139,7 @@ int parse_strace_line(const char *line, SyscallInfo *info) {
   regex_t regex;
   regmatch_t matches[MAX_MATCHES];
   int ret;
-  char *pattern = "^([[:alnum:]]+)\\(.*\\)\\s*=\\s*-?[[:digit:]]+\\s*<([[:digit:].]+)>$";
+  char *pattern = "^([[:alnum:]]+).*<([[:digit:].]+)>$";
 
   // 编译正则表达式
   if (regcomp(&regex, pattern, REG_EXTENDED)) {
