bf6e5a3d5f296945e22b2ec3d4cb68bdbb1d0084
tracer-nju <tracer@nju.edu.cn>
2025-03-20 20:13:04 +0800

    Linux LAPTOP-B1DV45O5 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:13:04 up 28 min,  1 user,  load average: 0.10, 0.10, 0.08

---
+++ .shadow/fsrecov/fsrecov.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include "fat32.h"
+
+struct fat32hdr *hdr;
+
+void *mmap_disk(const char *fname);
+void dfs_scan(u32 clusId, int depth, int is_dir);
+
+int main(int argc, char *argv[]) {
+
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s fs-image\n", argv[0]);
+        exit(1);
+    }
+
+    setbuf(stdout, NULL);
+
+    assert(sizeof(struct fat32hdr) == 512);
+    assert(sizeof(struct fat32dent) == 32);
+
+    // Map disk image to memory.
+    // The file system is a in-memory data structure now.
+    hdr = mmap_disk(argv[1]);
+
+    // File system traversal.
+    // dfs_scan(hdr->BPB_RootClus, 0, 1);
+
+    munmap(hdr, hdr->BPB_TotSec32 * hdr->BPB_BytsPerSec);
+}
+
+void *mmap_disk(const char *fname) {
+    int fd = open(fname, O_RDWR);
+
+    if (fd < 0) {
+        goto release;
+    }
+
+    off_t size = lseek(fd, 0, SEEK_END);
+    if (size < 0) {
+        goto release;
+    }
+
+    struct fat32hdr *hdr = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
+    if (hdr == MAP_FAILED) {
+        goto release;
+    }
+
+    close(fd);
+
+    assert(hdr->Signature_word == 0xaa55); // this is an MBR
+    assert(hdr->BPB_TotSec32 * hdr->BPB_BytsPerSec == size);
+
+    printf("%s: DOS/MBR boot sector, ", fname);
+    printf("OEM-ID \"%s\", ", hdr->BS_OEMName);
+    printf("sectors/cluster %d, ", hdr->BPB_SecPerClus);
+    printf("sectors %d, ", hdr->BPB_TotSec32);
+    printf("sectors %d, ", hdr->BPB_TotSec32);
+    printf("sectors/FAT %d, ", hdr->BPB_FATSz32);
+    printf("serial number 0x%x\n", hdr->BS_VolID);
+    return hdr;
+
+release:
+    perror("map disk");
+    if (fd > 0) {
+        close(fd);
+    }
+    exit(1);
+}
