24f3b76412b85f1298dac8e8b6d227eed28bcde8
tracer-nju <tracer@nju.edu.cn>
2025-03-21 18:47:11 +0800

    Linux LAPTOP-B1DV45O5 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:47:11 up  9:42,  1 user,  load average: 0.26, 0.17, 0.11

--- .shadow/fsrecov/fsrecov.c
+++ .shadow/fsrecov/fsrecov.c
@@ -349,7 +349,7 @@ int scan_dents_in_cluster(int clusId, clusterInfo *clusters,FILE* fp) {
         printf("bmp file sha1: %s\n", bmpf.sha1);
         get_longname(dent, &bmpf);
         printf("dent long name[%-12s]   ", bmpf.longname);
-        fprintf(fp, "%s %s\n", bmpf.sha1, bmpf.longname);
+        fprintf(fp, "%s  %s\n", bmpf.sha1, bmpf.longname);
       }
     }
     dent++;
